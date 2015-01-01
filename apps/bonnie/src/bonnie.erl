bdd09f3c08020fd429bdb11cce7b1c89ba3783c4
Aaron Seigo <aseigo@kde.org>
2015-01-01 23:29:20 +0100

    set the (provisional) mnesia location and start the server here

--- apps/bonnie/src/bonnie.erl
+++ apps/bonnie/src/bonnie.erl
@@ -12,7 +12,12 @@ start() -> application:start(bonnie).
 %% ===================================================================
 
 start(_StartType, _StartArgs) ->
+    %%TODO:
+    %%  - set the mnesia storage path
+    %%  - install the mnesia schema
     error_logger:info_msg("Starting Bonnie ... ~n"),
+    application:set_env(mnesia, dir, "db"),
+    application:start(mnesia),
     bonnie_sup:start_link().
 
 stop(_State) ->
