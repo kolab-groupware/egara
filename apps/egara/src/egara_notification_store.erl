b4718a696ef7fea28f368b127555774291e712bf
Aaron Seigo <aseigo@kde.org>
2015-01-05 15:33:46 +0100

    license headers

--- apps/egara/src/egara_notification_store.erl
+++ apps/egara/src/egara_notification_store.erl
@@ -1,3 +1,20 @@
+%% Copyright 2014 Kolab Systems AG (http://www.kolabsys.com)
+%%
+%% Aaron Seigo (Kolab Systems) <seigo a kolabsys.com>
+%%
+%% This program is free software: you can redistribute it and/or modify
+%% it under the terms of the GNU General Public License as published by
+%% the Free Software Foundation, either version 3 of the License, or
+%% (at your option) any later version.
+%%
+%% This program is distributed in the hope that it will be useful,
+%% but WITHOUT ANY WARRANTY; without even the implied warranty of
+%% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%% GNU General Public License for more details.
+%%
+%% You should have received a copy of the GNU General Public License
+%% along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 -module(egara_notification_store).
 -export([ install/1, start/0,
           notification/1, next_unnasigned/0,
