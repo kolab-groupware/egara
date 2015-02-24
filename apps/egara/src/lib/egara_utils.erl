2e965888815156d07629093e6f3cd1833fd9688c
Aaron Seigo <aseigo@kde.org>
2015-02-24 18:29:01 +0100

    start a little collection; for now just timestamps

---
+++ apps/egara/src/lib/egara_utils.erl
@@ -0,0 +1,25 @@
+%% Copyright 2014 Kolab Systems AG (http://www.kolabsys.com)
+%%
+%% Aaron Seigo (Kolab Systems) <seigo a kolabsys.com>
+%%
+%% This program is free software: you can redistribute it and/or modify
+%% it under the terms of the GNU General Public License as published by
+%% the Free Software Foundation, either version 3 of the License, or
+%% (at your option) any later version.
+%%
+%% This program is distributed in the hope that it will be useful,
+%% but WITHOUT ANY WARRANTY; without even the implied warranty of
+%% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%% GNU General Public License for more details.
+%%
+%% You should have received a copy of the GNU General Public License
+%% along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+-module(egara_utils).
+-export([ current_timestamp/0 ]).
+
+current_timestamp() ->
+    TS = { _ , _, Micro } = os:timestamp(),
+    { { Year,Month,Day }, { Hour,Minute,Second } } = calendar:now_to_universal_time(TS),
+    io_lib:format("~4w-~2..0w-~2..0wT~2w:~2..0w:~2..0w.~6..0w", [Year, Month, Day, Hour, Minute, Second, Micro]).
+
