b3de0fca5396444463b1b0357dd1e60b068839a9
Aaron Seigo <aseigo@kde.org>
2015-03-02 13:03:55 +0100

    formalize the riak a config a bit more, and add bucket type tweaking

--- apps/egara/src/egara_riak_config.erl
+++ apps/egara/src/egara_riak_config.erl
@@ -26,7 +26,7 @@
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
 
 %% state record definition
--record(state, { pos = 1, config }).
+-record(state, { pos = 1, nodes }).
 
 
 %% public API
@@ -35,11 +35,18 @@ connection_params() -> gen_server:call(?MODULE, connection_params).
 
 %% gen_server API
 init(_Args) -> 
-    { ok, #state{ config = sanitize_config(application:get_env(egara, riak, missing)) } }.
+    Config = application:get_env(egara, riak, []),
+    Nodes = sanitize_node_config(proplists:get_value(nodes, Config, missing)),
+    State = #state{ nodes = Nodes },
+    BucketTypes = proplists:get_value(bucket_types, Config, []),
+    { Host, Port } = erlang:hd(Nodes),
+    { ok, Connection } = riakc_pb_socket:start_link(Host, Port),
+    setup_bucket_types(Connection, BucketTypes),
+    riakc_pb_socket:stop(Connection),
+    { ok, State }.
 
 handle_call(connection_params, _From, State) ->
-    Length = length(State#state.config),
-    next_config(Length, State);
+    next_node(State);
 handle_call(_, _, State) -> { reply, ok, State}.
 
 handle_cast(_Msg, State) -> { noreply, State }.
@@ -51,19 +58,34 @@ terminate(_Reason, _State) -> ok.
 code_change(_OldVsn, State, _Extra) -> { ok, State }.
 
 %% private API
-sanitize_config(List) when is_list(List) ->
+sanitize_node_config(List) when is_list(List) ->
     Sanitizer = fun({ Host, Port } = Tuple, Acc) when is_list(Host), is_integer(Port) -> [ Tuple | Acc ];
                    (_, Acc) -> Acc
                 end,
     case lists:foldl(Sanitizer, [], List) of
-        [] -> sanitize_config(missing);
+        [] -> sanitize_node_config(missing);
         Config -> Config
     end;
-sanitize_config(_) -> [ { "localhost", 10017 } ].
+sanitize_node_config(missing) -> [ { "localhost", 10017 } ].
 
-next_config(Length, #state{ pos = Pos, config = Config } = State) when Pos > Length ->
-    [ Tuple | _ ] = Config,
+next_node(State) when is_record(State, state) -> next_node(length(State#state.nodes), State).
+next_node(Length, #state{ pos = Pos, nodes = Nodes } = State) when Pos > length(State#state.nodes) ->
+    [ Tuple | _ ] = Nodes,
     { reply, Tuple, State#state{ pos = 2 } };
-next_config(_Length, #state{ pos = Pos, config = Config } = State) ->
-    Tuple = lists:nth(Pos, Config),
+next_node(_Length, #state{ pos = Pos, nodes = Nodes } = State) ->
+    Tuple = lists:nth(Pos, Nodes),
     { reply, Tuple, State#state{ pos = Pos + 1 } }.
+
+%%TODO: current almost entirely useless since the API does not allow creating new bucket types
+setup_bucket_types(Connection, [{ Bucket, Properties } | Tail]) -> 
+    CurrentTypeProperties = riakc_pb_socket:get_bucket_type(Connection, Bucket),
+    case CurrentTypeProperties of
+        { ok, _ } -> %% TODO check that the properties actually match up so as not to set the type up EVERY time
+            riakc_pb_socket:set_bucket_type(Connection, Bucket, Properties);
+        { error, _ } ->
+            lager:info("Missing Riak bucket type ~p, intended to have properties ~p", [Bucket, CurrentTypeProperties]),
+            riakc_pb_socket:set_bucket_type(Connection, Bucket, Properties)
+    end,
+    setup_bucket_types(Connection, Tail);
+setup_bucket_types(_Connection, []) -> ok.
+
