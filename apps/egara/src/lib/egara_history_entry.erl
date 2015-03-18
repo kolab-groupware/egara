371fd605dec0570c9134ae99c87a02df47f93479
Aaron Seigo <aseigo@kde.org>
2015-03-18 15:02:04 +0100

    add groupware uid to the history bodies as well

--- apps/egara/src/lib/egara_history_entry.erl
+++ apps/egara/src/lib/egara_history_entry.erl
@@ -6,13 +6,13 @@ store(_Storage, _Timestamp, _GroupwareUid, _NewFolderUid, none, _OldFolderUid, _
 store(Storage, Timestamp, GroupwareUid, NewFolderUid, NewMessageUid, OldFolderUid, none) ->
     NewMessageUidBin = integer_to_binary(NewMessageUid),
     Key = generate_history_key(Storage, Timestamp, GroupwareUid, NewFolderUid, NewMessageUidBin),
-    ValueJson = <<"{ \"history\": {} }">>,
+    ValueJson = <<"{ \"groupware_uid\": ", GroupwareUid/binary, " \"history\": {} }">>,
     egara_storage:store_message_history_entry(Storage, Key, ValueJson);
 store(Storage, Timestamp, GroupwareUid, NewFolderUid, NewMessageUid, OldFolderUid, OldMessageUid) ->
     NewMessageUidBin = integer_to_binary(NewMessageUid),
     OldMessageUidBin = integer_to_binary(OldMessageUid),
     Key = generate_history_key(Storage, Timestamp, GroupwareUid, NewFolderUid, NewMessageUidBin),
-    ValueTerm = [{ <<"history">>, [{ <<"imap">>, [{ <<"previous_id">>, OldMessageUidBin }, { <<"previous_folder">>, OldFolderUid }] }] }],
+    ValueTerm = [{<<"groupware_uid">>, GroupwareUid }, { <<"history">>, [{ <<"imap">>, [{ <<"previous_id">>, OldMessageUidBin }, { <<"previous_folder">>, OldFolderUid }] }] }],
     ValueJson = jsx:encode(ValueTerm),
     egara_storage:store_message_history_entry(Storage, Key, ValueJson).
 
