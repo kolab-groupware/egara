04e340a194c100d86addbe4d5953f203e1a2ca8b
Thomas Bruederli <bruederli@kolabsys.com>
2015-03-19 03:20:32 +0100

    Fix json format in timeline records (#4878)

--- apps/egara/src/lib/egara_history_entry.erl
+++ apps/egara/src/lib/egara_history_entry.erl
@@ -6,7 +6,7 @@ store(_Storage, _Timestamp, _GroupwareUid, _NewFolderUid, none, _OldFolderUid, _
 store(Storage, Timestamp, GroupwareUid, NewFolderUid, NewMessageUid, OldFolderUid, none) ->
     NewMessageUidBin = integer_to_binary(NewMessageUid),
     Key = generate_history_key(Storage, Timestamp, GroupwareUid, NewFolderUid, NewMessageUidBin),
-    ValueJson = <<"{ \"groupware_uid\": ", GroupwareUid/binary, " \"history\": {} }">>,
+    ValueJson = <<"{ \"groupware_uid\": \"", GroupwareUid/binary, "\", \"history\": {} }">>,
     egara_storage:store_message_history_entry(Storage, Key, ValueJson);
 store(Storage, Timestamp, GroupwareUid, NewFolderUid, NewMessageUid, OldFolderUid, OldMessageUid) ->
     NewMessageUidBin = integer_to_binary(NewMessageUid),
