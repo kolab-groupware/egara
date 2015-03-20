f58588b10994e23ca1f71bfb62e96165af4e0cf3
Aaron Seigo <aseigo@kde.org>
2015-03-20 16:12:14 +0100

    do this the safe way: jsx.
    removes all the edge cases.

--- apps/egara/src/lib/egara_history_entry.erl
+++ apps/egara/src/lib/egara_history_entry.erl
@@ -6,7 +6,8 @@ store(_Storage, _Timestamp, _GroupwareUid, _NewFolderUid, none, _OldFolderUid, _
 store(Storage, Timestamp, GroupwareUid, NewFolderUid, NewMessageUid, OldFolderUid, none) ->
     NewMessageUidBin = integer_to_binary(NewMessageUid),
     Key = generate_history_key(Storage, Timestamp, GroupwareUid, NewFolderUid, NewMessageUidBin),
-    ValueJson = <<"{ \"groupware_uid\": \"", GroupwareUid/binary, "\", \"history\": {} }">>,
+    ValueTerm = [{ <<"groupware_uid">>, GroupwareUid }, { <<"history">>, [] }],
+    ValueJson = jsx:encode(ValueTerm),
     egara_storage:store_message_history_entry(Storage, Key, ValueJson);
 store(Storage, Timestamp, GroupwareUid, NewFolderUid, NewMessageUid, OldFolderUid, OldMessageUid) ->
     NewMessageUidBin = integer_to_binary(NewMessageUid),
